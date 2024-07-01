6553193c73fd13d6a8ddb0127300c2fa37204abf
Aapush01 <mdshahidafridia31@gmail.com>
2024-07-01 10:35:09 +0530

    add components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 function App() {
-
   return (
-    <div>
-        Start building this app.
-        lets check 
-    </div>
+    <>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
-
-export default App
